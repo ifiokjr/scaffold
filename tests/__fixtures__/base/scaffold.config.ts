c7bcc06533f1f31098ca68b766c384cbdec36c55
Ifiok Jr <ifiokotung@gmail.com>
2022-06-23 13:06:47 +0100

    test: local project scaffolding

--- tests/__fixtures__/base/scaffold.config.ts
+++ tests/__fixtures__/base/scaffold.config.ts
@@ -1,18 +1,22 @@
 import { defineTemplate } from "../../../mod.ts";
+import { path } from "../../../src/deps/path.ts";
 
 export default defineTemplate({
   getVariables() {
     return {
       name: "scaffold",
       description: "A scaffold for Deno",
+      version: '1.0.0',
     };
   },
   getPermissions() {
-    return { env: ["git", "pnpm"] };
-  },
-  getInstallCommand() {
-    return () => {
-      console.log("awesome!");
+    return {
+      // env: ["git", "pnpm"]
     };
+  },
+  getExcluded: ['ignore-me.md'],
+  getRenamed: { './rename-me.md': './rename-thee.md' },
+  getInstallCommand({ destination }) {
+    return async () => await Deno.writeTextFile(path.join(destination, 'hello.text'), 'hello world')
   },
 });
--- tests/scaffold_test.ts
+++ tests/scaffold_test.ts
@@ -1,6 +1,22 @@
+import { glob } from "../mod.ts";
+import { path } from "../src/deps/path.ts";
 import { uint8ArrayToString } from "../src/utils.ts";
-import { assertSnapshot, describe, it } from "./deps.ts";
-import { run } from "./helpers.ts";
+import {
+  afterAll,
+  assertEquals,
+  assertSnapshot,
+  describe,
+  it,
+} from "./deps.ts";
+import { cwd, run } from "./helpers.ts";
+
+const needRemoval: string[] = [];
+
+afterAll(async () => {
+  for (const file of needRemoval) {
+    await Deno.remove(file, { recursive: true });
+  }
+});
 
 describe("scaffold", () => {
   it("can display help", async (t) => {
@@ -9,5 +25,43 @@ describe("scaffold", () => {
     command.close();
 
     await assertSnapshot(t, output);
+  });
+
+  it("can display the version", async () => {
+    const command = run(["-V"], { stdout: "piped" });
+    const output = uint8ArrayToString(await command.output()).trim();
+    command.close();
+
+    assertEquals(output, "0.0.0");
+  });
+
+  it.only("can create templates from local folders", async (t) => {
+    const target = path.join(
+      cwd,
+      "tests/__fixtures__/tmp",
+      crypto.randomUUID(),
+    );
+    needRemoval.push(target);
+    const command = run(["./tests/__fixtures__/base", target], {
+      stdout: "piped",
+      stderr: "piped",
+    });
+
+    const [_, __] = await Promise.all([
+      command.output(),
+      command.stderrOutput(),
+    ]);
+    // console.info(uint8ArrayToString(output));
+    // console.info(uint8ArrayToString(error));
+    command.close();
+    const iterator = glob({ cwd: target, dot: true, junk: true });
+    const files: Record<string, string> = Object.create(null);
+
+    for await (const file of iterator) {
+      if (file.isDirectory) continue;
+      files[file.relative] = await Deno.readTextFile(file.absolute);
+    }
+
+    await assertSnapshot(t, files);
   });
 });
